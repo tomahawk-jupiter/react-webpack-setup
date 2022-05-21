3f4e21f223da95f25efb3e242ee74b42fbba96de
jupiter <brianbrainrogers@gmail.com>
2022-05-21 20:55:21 +0100

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./app.sass";
+
+const App = () => {
+  return (
+    <>
+      <h1>React Boilerplate</h1>
+      <p>
+        Can recognize JSX files extensions. This should be green if sass / css
+        loaded!
+      </p>
+    </>
+  );
+};
+
+export default App;
